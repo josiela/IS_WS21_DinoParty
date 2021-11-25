86f300c6172e4681765fc553011a8bb6c9bf7799
Mona <mona.vonhein@web.de>
2021-11-25 20:47:04 +0100

    oh oh

--- scripts/Ralf.gd
+++ scripts/Ralf.gd
@@ -17,8 +17,6 @@ var isOnWall : bool=false
 var direction= 1
 var wallJumpForce=500
 var justWallJumped= false
-var wall_slide_speed= 10
-var wall_slide_gravity = 10
 var setHighscore = true
 
 var movement= Vector2(500,0)
@@ -33,53 +31,46 @@ var gravity= 90
 func _physics_process(delta):
 	
 	var koords=get_position()
-	
-	if(koords.x==oldX):
-		print("Jo ich stehe")
+															#Score zählen und Pausieren
+	if(koords.x<=oldX ):
 		emit_signal("dontUpdateScorePlease")
 	else:
 		emit_signal("updateScorePlease")
 	oldX=koords.x
 	
-	
-
+		
 	if(lifespan>0):											#nur Wenn nicht tot
-	
+		print(movement.x)
 		movement = move_and_slide(movement, Vector2.UP)
 		movement.y = movement.y + gravity
 		_animated_sprite.play("Run")
 		
-		
+		if movement.x == 0:
+			movement.x = 500
 				
-		#about wallsliding and jumping and walljumping
+														 #about wallsliding and jumping and walljumping
 		if Input.is_action_just_pressed("jump"):
 			if(is_on_floor()):
+				#movement.x=500
 				movement.y= jumpforce
 				_animated_sprite.play("Stand")
 				
-			if(isOnWall):
-				
+			if(nextToWall() and not is_on_floor()):
 				movement.y= jumpforce
 				print("Wall Jump BAM")
 				if (direction==1):
-					$Sprite.flip_h=true
+					$AnimatedSprite.flip_h=true
 					movement.x=-wallJumpForce
+					
 				if (direction==-1):
-					$Sprite.flip_h=false
+					$AnimatedSprite.flip_h=false
 					movement.x=wallJumpForce
 				direction*=-1
-					
-		if (is_on_wall() and !is_on_floor()):
-			isOnWall=true
-			print("aaa")
-			
-		if (is_on_floor()):
-			
-			isOnWall=false
-			
+
+	
 		if (is_on_floor() && direction==-1):
 			movement.x=300
-			$Sprite.flip_h=false
+			$AnimatedSprite.flip_h=false
 			
 			
 			#alles mit Leben und Sterben				#Codereste, falls man mal collision braucht die nicht durchgehbar sein soll
@@ -117,19 +108,20 @@ func _set_lifespan(variable):
 	if(lifespan<=3):					#wenn man das Leben verändern will muss man _set_lifespan(1) oder -1 angeben
 		lifespan+=variable
 		emit_signal("lifespan_updated", lifespan)
-	
-
-func _on_Timer_timeout():
-	print("timeout")
-	get_tree().change_scene("res://DeadMenue.tscn")
-
 
 func _on_PartyHat1_area_entered(area):
 	_set_lifespan(1)
 	_animated_sprite.play("Hat")
 	print("The Hat is entered")
 
-
+func nextToWall():
+	return nextToRightWall() or nextToLeftWall()
+	
+func nextToRightWall():
+	return $RightWall.is_colliding()
+	
+func nextToLeftWall():
+	return $LeftWall.is_colliding()
 
 func _on_Enemy1_area_entered(area):
 	_set_lifespan(-1)
@@ -140,10 +132,12 @@ func _on_PartyHat1_area_exited(area):
 
 
 func _on_FallingDeathZone_area_entered(area):
-	_set_lifespan(-2)
+	print(lifespan)
+	lifespan=0
 
 
 func _on_Area2D_area_entered(area):
 	if get_tree().change_scene(endScreen_path) != OK:
 		# Error handling
 		print("Error: Unavailable endszene")
+		
